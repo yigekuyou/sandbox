68bdf32b100d9c041f6f4f2d9119aae8c5b18431
yigekuyou <47034094+yigekuyou@users.noreply.github.com>
2023-07-29 23:13:39 +0800

    Add files via upload

--- startqqnt.sh
+++ startqqnt.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/zsh
-
 CMD=$*
 QQdir=${CMD%% *}
 if [[ ${${CMD##* }##*\.} == appimage ]] {
@@ -16,7 +15,7 @@ chmod 700 /tmp/QQ
 cp $QQdir /tmp/QQ
 cd /tmp/QQ
 chmod +x /tmp/QQ/${QQdir##*\/}
-./${QQdir##*\/} --appimage-extract
+./${QQdir##*\/} --appimage-extract > /dev/null
 rm -f /tmp/QQ/${QQdir##*\/}
 if [[ -z "${QQ_DOWNLOAD_DIR}" ]] {
     if [[ -z "${XDG_DOWNLOAD_DIR}" ]] {
@@ -24,8 +23,8 @@ if [[ -z "${QQ_DOWNLOAD_DIR}" ]] {
         }
     QQ_DOWNLOAD_DIR="${XDG_DOWNLOAD_DIR:-$HOME/Downloads}"
     }
-    echo ${CMD##* }
 QQ_APP_DIR=/tmp/QQ/squashfs-root
+APPDIR=${QQ_APP_DIR}
 bwrap --new-session --cap-drop ALL --unshare-user-try --unshare-pid --unshare-cgroup-try --die-with-parent \
     --symlink usr/lib /lib \
     --symlink usr/lib64 /lib64 \
@@ -52,6 +51,5 @@ bwrap --new-session --cap-drop ALL --unshare-user-try --unshare-pid --unshare-cg
     --ro-bind-try "${HOME}/.local/share/.icons" "${HOME}/.local/share/.icons" \
     --ro-bind-try "${XDG_CONFIG_HOME}/gtk-3.0" "${XDG_CONFIG_HOME}/gtk-3.0" \
     --setenv IBUS_USE_PORTAL 1 \
-    --setenv APPDIR ${QQ_APP_DIR} \
-    ${QQ_APP_DIR}/AppRun ${CMD##* }
+    ${APPDIR}/AppRun ${CMD##* }
 rm -rf /tmp/QQ
