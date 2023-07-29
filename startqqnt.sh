3b154b6ae5ca15bf2bc7330d2eb3bceb7f4da5d8
yigekuyou <47034094+yigekuyou@users.noreply.github.com>
2023-07-29 22:31:17 +0800

    Add files via upload

--- startqqnt.sh
+++ startqqnt.sh
@@ -1,22 +1,30 @@
 #!/usr/bin/zsh
-QQdir=$*
+
+CMD=$*
+QQdir=${CMD%% *}
+if [[ ${${CMD##* }##*\.} == appimage ]] {
+    unset CMD
+    }
 if [[  ${QQdir##*\.} != appimage ]] {
-echo "only appimage"
+echo "only name./appimage"
 return -1
 }
 USER_RUN_DIR="/run/user/$(id -u)"
+trap 'rm -rf /tmp/QQ ; return 0' INT TERM
 mkdir /tmp/QQ
+chmod 700 /tmp/QQ
 cp $QQdir /tmp/QQ
 cd /tmp/QQ
 chmod +x /tmp/QQ/${QQdir##*\/}
 ./${QQdir##*\/} --appimage-extract
 rm -f /tmp/QQ/${QQdir##*\/}
-if [ -z "${QQ_DOWNLOAD_DIR}" ] {
-    if [ -z "${XDG_DOWNLOAD_DIR}" ] {
+if [[ -z "${QQ_DOWNLOAD_DIR}" ]] {
+    if [[ -z "${XDG_DOWNLOAD_DIR}" ]] {
         XDG_DOWNLOAD_DIR="$(xdg-user-dir DOWNLOAD)"
         }
     QQ_DOWNLOAD_DIR="${XDG_DOWNLOAD_DIR:-$HOME/Downloads}"
     }
+    echo ${CMD##* }
 QQ_APP_DIR=/tmp/QQ/squashfs-root
 bwrap --new-session --cap-drop ALL --unshare-user-try --unshare-pid --unshare-cgroup-try --die-with-parent \
     --symlink usr/lib /lib \
@@ -40,9 +48,10 @@ bwrap --new-session --cap-drop ALL --unshare-user-try --unshare-pid --unshare-cg
     --bind-try "${QQ_DOWNLOAD_DIR}" "${QQ_DOWNLOAD_DIR}" \
     --bind "${QQ_APP_DIR}" "${QQ_APP_DIR}" \
     --tmpfs /dev/shm  \
-    --ro-bind-try "${HOME}/.icons" "${HOME}/.icons" \
+    --ro-bind-try "${HOME}/.icons" "${HOME}/.icons" \${QQ_APP_DIR} \
     --ro-bind-try "${HOME}/.local/share/.icons" "${HOME}/.local/share/.icons" \
     --ro-bind-try "${XDG_CONFIG_HOME}/gtk-3.0" "${XDG_CONFIG_HOME}/gtk-3.0" \
     --setenv IBUS_USE_PORTAL 1 \
-    ${QQ_APP_DIR}/AppRun
+    --setenv APPDIR ${QQ_APP_DIR} \
+    ${QQ_APP_DIR}/AppRun ${CMD##* }
 rm -rf /tmp/QQ
