f5944bd1a03fcb31f9ad44ffc7d989b6265b906f
yigekuyou <47034094+yigekuyou@users.noreply.github.com>
2024-04-16 11:38:21 +0800

    Add files via upload

--- bwrap-napcat.sh
+++ bwrap-napcat.sh
@@ -38,31 +38,35 @@ NCqq="--ro-bind $LOAD/package.json ${QQ_APP_DIR}/resources/app/NapCat/package.js
 --ro-bind $LOAD/napcat.cjs ${QQ_APP_DIR}/resources/app/NapCat/napcat.cjs"
 }
 USER_RUN_DIR="/run/user/$(id -u)"
-Part="--clearenv --new-session --cap-drop ALL --unshare-user-try --unshare-pid --unshare-cgroup-try --die-with-parent \
+Part=" --new-session --cap-drop ALL --unshare-user-try --unshare-pid --unshare-cgroup-try --die-with-parent \
     --symlink usr/lib /lib \
     --symlink usr/lib64 /lib64 \
     --ro-bind /usr /usr \
     --ro-bind /usr/bin/ffprobe /bin/ffprobe  \
-    --ro-bind /usr/bin/bash /bin/bash \
     --ro-bind /usr/bin/zsh /bin/sh \
-    --ro-bind /etc/ld.so.cache /etc/ld.so.cache \
-    --ro-bind-try /etc/fonts /etc/fonts \
     --dev-bind /dev /dev \
     --ro-bind /sys /sys \
+    --ro-bind /etc/vulkan /etc/vulkan \
+    --ro-bind-try /etc/fonts /etc/fonts \
+    --ro-bind /etc/ld.so.cache /etc/ld.so.cache \
+    --ro-bind /etc/machine-id /etc/machine-id \
+    --ro-bind /etc/nsswitch.conf /etc/nsswitch.conf \
     --ro-bind /etc/passwd /etc/passwd \
     --ro-bind-try /run/systemd/userdb /run/systemd/userdb \
     --ro-bind /etc/resolv.conf /etc/resolv.conf \
     --ro-bind /etc/localtime /etc/localtime \
+    --tmpfs /dev/shm  \
     --proc /proc \
-    --ro-bind-try /etc/fonts /etc/fonts \
     --dev-bind /tmp /tmp \
     --tmpfs ${HOME}/.config/QQ \
     --bind "${QQ_APP_DIR}" "${QQ_APP_DIR}" \
     --tmpfs /dev/shm  \
+    --ro-bind /usr/bin/kdialog /bin/kdialog  \
     --setenv ELECTRON_RUN_AS_NODE 1 \
+    --setenv XNLSPATH /usr/X11R6/lib/X11/nls \
     --setenv APPDIR ${APPDIR} \
     ${NCqq} \
-    ${APPDIR}/qq ${QQ_APP_DIR}/resources/app/NapCat/napcat.cjs ${CMD#* }"
+     ${APPDIR}/qq  ${QQ_APP_DIR}/resources/app/NapCat/napcat.cjs ${CMD#* }"
     #strace -y -o /tmp/logs/log
 (timeout 30 tail -f -n0 /tmp/NCQQ/log |grep -q "qrcode.png"  && xdg-open $FIFO && ) &
 bwrap `echo $Part` >/tmp/NCQQ/log
