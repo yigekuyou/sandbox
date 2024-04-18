c9c655fad834441ed508f50b3f0c28e1b4b7d9b8
yigekuyou <47034094+yigekuyou@users.noreply.github.com>
2024-04-18 18:27:39 +0800

    Add files via upload

--- bwrap-napcat.sh
+++ bwrap-napcat.sh
@@ -7,9 +7,33 @@ XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 NCQQ=/tmp/NCQQ
 QQ_APP_DIR=${NCQQ}/squashfs-root
 APPDIR=${QQ_APP_DIR}
-napcatQQ=${QQ_APP_DIR}/resources/app/NapCat
+napcatQQ=${QQ_APP_DIR}/resources/app/NapCat${QQ}
 
-
+# 环境问题
+if [[ ! -d "${NCQQ}" ]] {
+# 从一大串arg提取key
+keyname=AppImage
+	if [[ ${${CMD##* }##*\.} == ${keyname} ]] {
+	#别动 不然启动后缀出现两次
+	unset CMD
+	}
+	#下面请随意
+	if [[  ${QQdir##*\.} != ${keyname} ]] {
+	echo "only name./AppImage"
+	return -1
+	}
+trap 'rm -rf ${NCQQ} ;return -1' INT
+mkdir ${NCQQ}
+chmod 700 ${NCQQ}
+cp $QQdir ${NCQQ}
+cd ${NCQQ}
+chmod +x ${NCQQ}/${QQdir##*\/}
+./${QQdir##*\/} --appimage-extract > /dev/null
+rm ${NCQQ}/${QQdir##*\/}
+rm -rf ${QQ_APP_DIR}/resources/app/fonts
+rm -rf ${QQ_APP_DIR}/{usr/lib,libEGL.so,libGLESv2.so,libvk_swiftshader.so,libvulkan.so.*}
+rm -f ${QQ_APP_DIR}/resources/app/{libssh2.so.1,libunwind*,sharp-lib/libvips-cpp.so.42}
+}
 function command_exists() {
 	local command="$1"
 	command -v "${command}" >/dev/null 2>&1
@@ -19,49 +43,44 @@ LOAD=$(realpath $(dirname $(readlink -f $0)) )
 CMD=$*
 QQdir=${CMD%% *}
 # 获取登录图片的方法
-LOG=${NCQQ}/log
+LOG=${NCQQ}/${QQ}/QQ/QQ${QQ}.log
 
-# 暂存日志
+# 别扫码了
+if [[ ${QQ} ]] {
+QQlogin="-q ${QQ}"
+[[ ! -e ${LOAD}/config/onebot11_${QQ} ]] && cp ${LOAD}/config/onebot11.json ${LOAD}/config/onebot11_${QQ}.json
+# 	debug喽
+	grep '"debug": ture' ${LOAD}/config/onebot11_${QQ}.json &&DEBUG=1
+	QQconfig="--dev-bind ${LOAD}/config/onebot11_${QQ}.json ${napcatQQ}/config/onebot11_${QQ}.json"
+	trap "rm -rf ${napcatQQ} ${NCQQ}/${QQ}/crash_files ; return -1"  TERM HUP INT
+} else { QQconfig="--dev-bind ${LOAD}/config ${napcatQQ}/config"
+trap "rm -rf ${napcatQQ} ${NCQQ}/QQ ; return -1"  TERM HUP INT
+}
+# 暂存日志 记得打开配置的debug
 if  [[ ${DEBUG} ]] {
-DEBUGLOG=/tmp/logs/QQ-diagnostic
+DEBUGLOG=/tmp/logs/QQ${QQ}-diagnostic
 if command_exists strace ;then
 DEBUGCMD="/usr/bin/strace -y -o ${DEBUGLOG}"
 fi
-LOG=/tmp/logs/QQ-log
+LOG=/tmp/logs/QQ${QQ}.log
 }
-
-
-if [[ ${${CMD##* }##*\.} == AppImage ]] {
-#	别动 不然启动后缀出现两次
-	unset CMD
-	}
-#	下面请随意
-if [[  ${QQdir##*\.} != AppImage ]] {
-echo "only name./AppImage"
-return -1
-}
-# 你也不想没有clear env吧
-trap 'rm -rf ${NCQQ} ; return -1' INT TERM
-# 挂载哪些能保存登录信息而不受垃圾影响
-
-mkdir ${NCQQ}
-chmod 700 ${NCQQ}
-cp $QQdir ${NCQQ}
-cd ${NCQQ}
-chmod +x ${NCQQ}/${QQdir##*\/}
-./${QQdir##*\/} --appimage-extract > /dev/null
-rm ${NCQQ}/${QQdir##*\/}
-rm -rf ${QQ_APP_DIR}/resources/app/fonts
-rm -f ${QQ_APP_DIR}/resources/app/{libssh2.so.1,libunwind*,sharp-lib/libvips-cpp.so.42}
-
-if [[ -d "${LOAD}" ]] {
+# 初始化
+mkdir -p ${NCQQ}/${QQ}/QQ
 touch ${LOG}
+
+
+# 不是哥们 没这个就会死
+if [[ -d "${LOAD}/node_modules" ]] {
 FIFO="${napcatQQ}/qrcode.png"
 mkdir ${napcatQQ}
 NCqq="--ro-bind $LOAD/package.json ${napcatQQ}/package.json \
+${QQconfig} \
 --ro-bind $LOAD/node_modules ${napcatQQ}/node_modules \
---dev-bind $LOAD/config ${napcatQQ}/config \
 --ro-bind $LOAD/napcat.cjs ${napcatQQ}/napcat.cjs"
+} else {
+echo 提醒需要把启动脚本放入napcat的根目录 \n
+[[ -e ${LOAD}/README.md ]]&&echo "还是说没有node_modules？ \n 帮你打开看看" &&xdg-open ${LOAD}/README.md
+return -1
 }
 USER_RUN_DIR="/run/user/$(id -u)"
 #	这是我的一小步
@@ -84,20 +103,17 @@ Part="--new-session --cap-drop ALL --unshare-user-try --unshare-pid --unshare-cg
 	--ro-bind /etc/localtime /etc/localtime \
 	--tmpfs /dev/shm  \
 	--proc /proc \
+	--tmpfs ${HOME}/.config \
 	--dev-bind /tmp /tmp \
-	--tmpfs ${HOME}/.config/QQ \
+	--dev-bind ${NCQQ}/${QQ}/QQ ${HOME}/.config/QQ \
 	--bind "${QQ_APP_DIR}" "${QQ_APP_DIR}" \
 	--tmpfs /dev/shm  \
 	--setenv ELECTRON_RUN_AS_NODE 1 \
 	--setenv  PATH  /bin \
 	--setenv APPDIR ${APPDIR} \
 	${NCqq} \
-	${DEBUGCMD} ${APPDIR}/qq  ${napcatQQ}/napcat.cjs ${CMD#* }"
+	${DEBUGCMD} ${APPDIR}/qq  ${napcatQQ}/napcat.cjs ${QQlogin} ${CMD#* }"
 	#子进程监听任务
-(timeout 30 tail -f -n0 ${LOG} |grep -q "qrcode.png"  && xdg-open $FIFO &&timeout 120 tail -f -n0 ${LOG} |grep -q "onQRCodeSessionFailed 1"&&pkill -9 -P $$ ) &
-
+(timeout 30 tail -f -n0 ${LOG} |grep -q "qrcode.png"  && xdg-open $FIFO &&timeout 120 tail -f -n0 ${LOG} |grep -q "onQRCodeSessionFailed 1"&&pkill -15 -P $$ ) &
 # 主进程
-(bwrap `echo $Part` &>${LOG}) &
-tail -f -n0 ${LOG} |grep -q "FATAL ERROR"&&pkill -9 -P $$
-rm -rf  ${NCQQ}
-return 0
+bwrap `echo $Part` &>${LOG} ;pkill -TERM -P $$
