3a4420d4503e554a2896f6bd5407291973015ecf
yigekuyou <47034094+yigekuyou@users.noreply.github.com>
2024-05-13 21:20:12 +0800

    Add files via upload

--- startqqnt.sh
+++ startqqnt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
-
-USER_RUN_DIR="/run/user/$(id -u)"
+APP_NAME=com.qq.QQ
+APP_FOLDER="$XDG_RUNTIME_DIR/app/$APP_NAME"
 XAUTHORITY="${XAUTHORITY:-$HOME/.Xauthority}"
 XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 FONTCONFIG_HOME="${XDG_CONFIG_HOME}/fontconfig"
@@ -16,17 +16,6 @@ LOAD=$(dirname $(readlink -f $0))/LiteLoaderQQNT
 CMD=$*
 QQdir=${CMD%% *}
 
-QQ_HOTUPDATE_DIR="${QQ_APP_DIR}/versions"
-QQ_HOTUPDATE_VERSION="${QQ_HOTUPDATE_DIR}/${QQ_HOTUPDATE_VERSION}"
-HOTUPDATE="--ro-bind $APPDIR/resources/app ${QQ_HOTUPDATE_DIR}"
-
-if [[ -z "${QQ_DOWNLOAD_DIR}" ]] {
-	if [[ -z "${XDG_DOWNLOAD_DIR}" ]] {
-		XDG_DOWNLOAD_DIR="$(xdg-user-dir DOWNLOAD)"
-		}
-	QQ_DOWNLOAD_DIR="${XDG_DOWNLOAD_DIR:-$HOME/Downloads}"
-	}
-
 
 # 环境问题
 if [[ ! -d ${LOGIN} ]] {
@@ -39,7 +28,7 @@ keyname=AppImage
 	if [[ ${${CMD##* }##*\.} == ${keyname} ]] {
 	#别动 不然启动后缀出现两次
 	unset CMD
-
+}
 
 if [[ ! -d "${QQ_APP_DIR}" ]] {
 # 从一大串arg提取key
@@ -50,7 +39,7 @@ if [[ ! -d "${QQ_APP_DIR}" ]] {
 	return -1
 	}
 
-	trap "rm -rf ${QQ_APP_DIR} ;return -1"INT
+	trap "rm -rf ${QQ_APP_DIR} ${APP_FOLDER} ;return -1"INT
 	cp $QQdir ${QQ_APP_ROOTDIR}
 	cd ${QQ_APP_ROOTDIR}
 	chmod +x ${QQ_APP_ROOTDIR}/${QQdir##*\/}
@@ -59,8 +48,14 @@ if [[ ! -d "${QQ_APP_DIR}" ]] {
 
 	rm -rf ${QQ_APP_DIR}/resources/app/fonts
 	rm -f ${QQ_APP_DIR}/resources/app/{libssh2.so.1,libunwind*,sharp-lib/libvips-cpp.so.42}
-	mkdir ${QQ_HOTUPDATE_DIR}
+	rm -rf ${QQ_APP_DIR}/{usr/lib,libvulkan.so.1,libvk_swiftshader.so}
+	mkdir -p "$APP_FOLDER"
+	echo "[Application]
+name=$APP_NAME" > ${QQ_APP_DIR}/flatpak-info
+
+	mkdir ${QQ_APP_DIR}/resources/app/LiteLoader
 }
+
 
 trap "exit -1" INT
 
@@ -71,7 +66,6 @@ function command_exists() {
 }
 
 if [[ -d "${LOAD}" ]] {
-	mkdir ${QQ_APP_DIR}/resources/app/LiteLoader
 	Config="--dev-bind $LOAD/data/data ${QQ_APP_DIR}/resources/app/LiteLoader/data/data"
 	if [[ -f $LOAD/data/data/LiteLoader/config.json  ]] {
 		pushd $LOAD/data/data
@@ -100,50 +94,87 @@ if [[ -d "${LOAD}" ]] {
 	--dev-bind $LOAD/data/config.json ${QQ_APP_DIR}/resources/app/LiteLoader/data/config.json \
 	--ro-bind /etc/ssl /etc/ssl \
 	--setenv LITELOADERQQNT_PROFILE ${QQ_APP_DIR}/resources/app/LiteLoader/data"
-	grep -q $LOAD ${QQ_APP_DIR}/resources/app/app_launcher/index.js|| sed -i "1 i require(\"${QQ_APP_DIR}/resources/app/LiteLoader/\");"  ${QQ_APP_DIR}/resources/app/app_launcher/index.js
+	grep -q LiteLoader ${QQ_APP_DIR}/resources/app/app_launcher/index.js|| sed -i "1 i require(\"${QQ_APP_DIR}/resources/app/LiteLoader/\");"  ${QQ_APP_DIR}/resources/app/app_launcher/index.js
+} else { grep -q LiteLoader ${QQ_APP_DIR}/resources/app/app_launcher/index.js&&sed -i "1d"  ${QQ_APP_DIR}/resources/app/app_launcher/index.js }
+
+Wayland="--enable-wayland-ime  --ozone-platform-hint=wayland  --enable-features=WaylandWindowDecorations"
+
+	set_up_dbus_proxy() {
+	bwrap \
+	--new-session \
+	--symlink /usr/lib64 /lib64 \
+	--ro-bind /usr/lib /usr/lib \
+	--ro-bind /usr/lib64 /usr/lib64 \
+	--ro-bind /usr/bin /usr/bin \
+	--bind "$XDG_RUNTIME_DIR" "$XDG_RUNTIME_DIR" \
+	--ro-bind ${QQ_APP_DIR}/flatpak-info "/.flatpak-info" \
+	--die-with-parent \
+	-- \
+	env -i xdg-dbus-proxy \
+	"$DBUS_SESSION_BUS_ADDRESS" \
+	"$APP_FOLDER/bus" \
+	--filter \
+	--log \
+	--own=$APP_NAME \
+	--talk=org.freedesktop.portal.Notification.AddNotification \
+	--talk=org.kde.StatusNotifierItem \
+	--talk=org.freedesktop.DBus.GetNameOwner \
+	--talk=org.freedesktop.Notifications \
+	--talk=org.kde.StatusNotifierWatcher \
+	--talk=StatusNotifierItem \
+	--talk=org.freedesktop.DBus.NameAcquired \
+	--talk=org.gtk.vfs.Daemon
+
 }
-
-Wayland="--enable-wayland-ime --setenv --ozone-platform-hint=wayland  --enable-features=WaylandWindowDecorations"
-
-
-USER_RUN_DIR="/run/user/$(id -u)"
-Part="--new-session --cap-drop ALL --unshare-user-try --unshare-pid --unshare-cgroup-try --die-with-parent \
+xauth=`echo ${XDG_RUNTIME_DIR}/xauth_*`
+Part="--new-session --unshare-all --share-net  --die-with-parent \
 	--symlink usr/lib /lib \
 	--symlink usr/lib64 /lib64 \
-	--ro-bind /usr /usr \
+	--ro-bind /usr/lib /usr/lib \
+	--ro-bind /usr/lib64 /usr/lib64 \
+	--ro-bind /usr/bin /usr/bin \
 	--ro-bind /usr/bin/flatpak-xdg-open /bin/xdg-open \
-	--ro-bind /usr/bin/kdialog /bin/kdialog  \
+	--ro-bind ${QQ_APP_DIR}/flatpak-info "/.flatpak-info" \
+	--ro-bind /usr/share /usr/share \
 	--ro-bind /usr/bin/bash /bin/bash \
 	--ro-bind /usr/bin/zsh /bin/sh \
-	--ro-bind /etc/ld.so.cache /etc/ld.so.cache \
 	--ro-bind /etc/machine-id /etc/machine-id \
 	--ro-bind /etc/nsswitch.conf /etc/nsswitch.conf \
 	--ro-bind-try /run/systemd/userdb /run/systemd/userdb \
-	--ro-bind /etc/resolv.conf /etc/resolv.conf \
+	--hostname lighthomo \
 	--ro-bind /etc/localtime /etc/localtime \
 	--ro-bind /etc/vulkan /etc/vulkan \
 	--ro-bind-try /etc/fonts /etc/fonts \
-	--dev-bind /dev /dev \
+	--dev /dev \
+	--ro-bind /sys/dev/char /sys/dev/char \
+	--ro-bind /sys/devices /sys/devices \
 	--ro-bind /sys /sys \
 	--ro-bind /etc/passwd /etc/passwd \
 	--ro-bind-try /run/systemd/userdb /run/systemd/userdb \
-	--ro-bind /etc/resolv.conf /etc/resolv.conf \
 	--ro-bind /etc/localtime /etc/localtime \
 	--dev-bind ${LOGIN} ${HOME}/.config/QQ \
 	--tmpfs ${HOME}/.config/QQ/crash_files \
+	--tmpfs ${HOME}/.config/QQ/Crashpad \
 	--proc /proc \
-	--dev-bind /run/dbus /run/dbus \
 	--ro-bind-try /etc/fonts /etc/fonts \
 	--dev-bind /tmp /tmp \
-	--bind "${USER_RUN_DIR}" "${USER_RUN_DIR}" \
-	--bind-try "${QQ_DOWNLOAD_DIR}" "${QQ_DOWNLOAD_DIR}" \
-	--bind "${QQ_APP_DIR}" "${QQ_APP_DIR}" \
-	--ro-bind-try "${FONTCONFIG_HOME}" "${FONTCONFIG_HOME}" \
+	--dev-bind "$APP_FOLDER" "${XDG_RUNTIME_DIR}" \
+	--ro-bind "${XDG_RUNTIME_DIR}/dconf" "${XDG_RUNTIME_DIR}/dconf" \
+	--ro-bind-try "${XDG_CONFIG_HOME}/dconf" "${XDG_CONFIG_HOME}/dconf" \
+	--ro-bind "${xauth}" "${xauth}" \
+	--ro-bind "$XDG_RUNTIME_DIR/pipewire-0" "$XDG_RUNTIME_DIR/pipewire-0" \
+	--dev-bind ${XDG_RUNTIME_DIR}/doc ${XDG_RUNTIME_DIR}/doc \
+	--ro-bind ${XDG_RUNTIME_DIR}/pulse ${XDG_RUNTIME_DIR}/pulse \
+	--ro-bind ${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY} ${XDG_RUNTIME_DIR}/${WAYLAND_DISPLAY} \
+	--dev-bind "${QQ_APP_DIR}" "${QQ_APP_DIR}" \
+	--ro-bind /lib64/libvulkan.so.1 ${QQ_APP_DIR}/libvulkan.so.1 \
 	--tmpfs /dev/shm  \
 	--ro-bind-try "${XDG_CONFIG_HOME}/gtk-3.0" "${XDG_CONFIG_HOME}/gtk-3.0" \
+	--setenv NO_AT_BRIDGE 1 \
 	--setenv APPDIR ${APPDIR} \
-	--setenv  PATH  /bin \
 	${LiteLoader} ${HOTUPDATE}   \
-	${APPDIR}/qq ${CMD#* } --ignore-gpu-blocklist ${Wayland}  --force-dark-mode --enable-features=WebUIDarkMode --enable-zero-copy"
+	${APPDIR}/qq ${CMD#* } --ignore-gpu-blocklist ${Wayland}  --force-dark-mode --enable-features=WebUIDarkMode --enable-zero-copy ${APPDIR}/resources/app"
 	#strace -y -o /tmp/logs/log
+set_up_dbus_proxy &
 bwrap `echo $Part`
+pkill -TERM -P $$
